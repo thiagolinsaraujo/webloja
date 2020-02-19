48e27ecd41e0a1e4562df665f3917701d1781553
Thiago Araújo <thiagolinsaraujo@gmail.com>
2020-02-18 22:42:35 -0300

    Criando o projeto em Spring Boot

---
+++ src/main/java/br/com/thiagoaraujo/webloja/WeblojaApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagoaraujo.webloja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeblojaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeblojaApplication.class, args);
+	}
+
+}
